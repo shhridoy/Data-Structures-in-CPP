5b7974abc3e3631f472436fd73d5ad273f9a3ede
SH Hridoy <shhridoy.cste@gmail.com>
2017-02-18 01:23:43 +0600

    sample linked list

---
+++ Linked List/Linked List.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node* next;
+};
+
+node* Head = NULL;
+void create(int value)
+{
+    node* nptr = new node;
+    nptr->data = value;
+    nptr->next = NULL;
+
+    if(Head == NULL){
+        Head = nptr;
+    }
+    else{
+        node* tptr = Head;
+        tptr->next = nptr;
+        tptr = nptr;
+    }
+}
+
+void print()
+{
+    cout<< "Current Linked List: ";
+    node* tptr = Head;
+    while(tptr != NULL){
+        cout<< tptr->data << "-> ";
+        tptr = tptr->next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    create(6);
+    print();
+    create(7);
+    print();
+    return 0;
+}
