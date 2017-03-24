52d7ef06e103f6e0edc432d5e51b811efc79971a
SH Hridoy <shhridoy.cste@gmail.com>
2017-03-24 10:17:52 +0600

    AVL tree insertion of node

--- Tree/AVL tree insertion.cpp
+++ Tree/AVL tree insertion.cpp
@@ -1,4 +1,8 @@
+/*AVL tree is a self-balancing Binary Search Tree (BST)
+where the difference between heights of left and right subtrees cannot be more than one for all nodes.
+*/
 /// C program to insert a node in AVL tree
+
 #include<stdio.h>
 #include<stdlib.h>
 
